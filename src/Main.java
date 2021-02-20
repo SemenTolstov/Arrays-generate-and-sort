0a551f71f6fee74e015bfcb5f25ea432f5bebec0
semen.tolstov@mail.ru <Semen.Tolstov@mail.ru>
2021-02-20 17:29:46 +0700

    initial project

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    private static final int dimension = 100;
+    public static void main(String[] args) {
+        generator(100);
+
+    }
+
+    public static void generator(Integer number) {
+        Set<Integer> size = new HashSet<>();
+        Integer[][] array = new Integer[number][];
+        for(int i = 0; i < number; i++) {
+            if (!size.contains(i)) {
+            array[i] = new Integer[(int)(Math.random() * dimension)];
+            size.add(i);
+            } else {
+                number++;
+                continue;
+            }
+            for (int j = 0; j < array[i].length; j++) {
+                (array[i])[j] = (int) (Math.random() * dimension);
+            }
+            if((i + 1) % 2 == 0) {
+                Arrays.sort(array[i]);
+            } else {
+                Arrays.sort(array[i], Collections.reverseOrder());
+            }
+            System.out.println(Arrays.toString(array[i]));
+        }
+    }
+
+}
